5d9ac7c90263aeae01bf7eca8d867779b171ef2a
MuizzRafid <91799156+MuizzRafid@users.noreply.github.com>
2023-12-31 14:07:31 +0000

    changes

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -13,13 +13,13 @@ function addTwoNumbers2(number1,number2){
     return number1+number2;
 }
 
-addTwoNumbers(3,5);//8
-addTwoNumbers(3,"4");//34
+// addTwoNumbers(3,5);//8
+// addTwoNumbers(3,"4");//34
 
-let result=addTwoNumbers(3,5);
-console.log("My result 1:",result);
+//let result=addTwoNumbers(3,5);
+//console.log("My result 1:",result);//My result 1:undefined
 result=addTwoNumbers2(3,5);
-console.log("My result 2:",result);
+//console.log("My result 2:",result);//My result 2:8
 
 
 function loginUserMessege(username){
@@ -30,5 +30,40 @@ function loginUserMessege(username){
     return `${username} just logged in`;
 }
 
-console.log(loginUserMessege("muizz"));
-console.log(loginUserMessege());
+// console.log(loginUserMessege("muizz"));//muizz just logged in
+// console.log(loginUserMessege());//please enter a username
+
+//rest operator(...) or spread operator(...) named different on diff uses
+
+//using rest operator(...)
+calculateCartPrice=function(val1, ...num1){
+return num1;
+}
+//console.log(calculateCartPrice(200,400,500,600));//[400,500,600]
+
+const user={
+    userName: "muizz",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.userName} and price is ${anyobject.price}`);
+}
+
+handleObject(user);
+
+//Passing direct object
+handleObject({
+    userName:"Downey",
+    price: 200
+});
+
+const myArr=[200,400,500,600];
+
+function handleArray(getArray){
+return getArray[2];
+}
+console.log(handleArray(myArr));
+
+//Passing direct Array
+console.log(handleArray([900,800,700,100]));
---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,55 @@
+
+var c=300;
+let a=100;
+{
+    
+    let a=10;
+    const b=20;
+    var c=30;
+    //console.log("inner a:",a);
+    
+}
+
+//console.log(a);//error
+//console.log(c);//i can print this outside of the scope and its a problem
+
+//console.log("outter a:",a);
+
+function one(){
+    let userName="Muizz";
+    function two(){
+        let email="mzrd1@gmail.com";
+        //console.log("call ones property inside two: ",userName);//sucessfully print
+    }
+    two();
+    return userName;
+}
+one();
+
+if(true){
+    const username="muizz ";
+    if(username.trim()==="muizz"){
+        const web="youtube";
+       // console.log(username+web);//muizz youtube
+    }
+   // console.log(web);//error
+}
+
+
+//***************       Interstring          ************
+//2ways declare function
+
+console.log(addOne(5));//no error for before declaration
+
+function addOne(num){
+    return num+1;
+}
+
+//addTwo();//gives error when call before declaration
+
+//this declaration also called expression
+const addTwo=function(num){
+return num+2;
+}
+
+//to learn more study hoisting
