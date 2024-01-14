a4dae004ca98a52ce917bece1001e3ceb52f755e
MuizzRafid <91799156+MuizzRafid@users.noreply.github.com>
2024-01-14 11:27:42 +0000

    c

--- 10_classes_and_oop/staticProp.js
+++ 10_classes_and_oop/staticProp.js
@@ -1,3 +1,5 @@
+//static helps to stop getting permisson to a function for objects
+
 class user{
     constructor(username){
         this.username=username;
@@ -7,12 +9,26 @@ class user{
         console.log(`Hey! ${this.username}`);
     }
 
-    createdID(){
+      createdID(){
         return `123`;
     }
 }
 
 
 const july=new user('july');
-july.callMe();
-//console.log(user.createdID());
+
+//console.log(july.createdID());//error
+
+class teacher extends user{
+  constructor(username,email){
+    super(username);
+    this.email=email;
+  }
+
+}
+
+const t=new teacher('teacher','t@gmail.com');
+
+t.callMe();//Hey! teacher
+
+console.log(t.createdID());
