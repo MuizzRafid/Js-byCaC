686c7faa2b421ccc85c0c1675ae2cda99dda22c5
Muizz Rafid <muizzrafid1@gmail.com>
2024-01-18 23:16:31 +0600

    geeks article added

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,7 +3,9 @@ const accountId=1232;
 let accountGmail="muizzrafid1@gmail.com";
 
 accountCity="panchagarh";
-
+//an undeclared variable become global also if it in scope
 console.log(accountId);
 
 console.table([accountId,accountGmail,accountCity]);
+
+// ** https://www.geeksforgeeks.org/javascript-variables/?ref=roadmap
---
+++ practice.js
@@ -0,0 +1,7 @@
+function letHost(){
+  a=20;
+  let b=10;
+}
+
+letHost();
+console.log(a);
