6eb66e9cc114dd34413d06b27b81bf3563a69953
Muizz Rafid <muizzrafid1@gmail.com>
2024-02-01 00:05:49 +0600

    info added 05's four.js

---
+++ 05_iterators/dist/four.dev.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// ****for in loop
+// forin in object
+var myObj = {
+  js: "JavaScript",
+  cpp: "C Plus Plus"
+};
+
+for (var key in myObj) {
+  //key is a array so i use myObj[key]
+  console.log("".concat(key, " means ").concat(myObj[key]));
+}
+/*
+console.log(myObj.key)//gives undefined cause there has not key as named "key"
+
+
+If you try to use myObj.key instead, JavaScript will attempt to access a property
+named "key" on the myObj object, rather than dynamically accessing the property 
+whose name is held in the variable key. Since there's no such property named "key"
+in your object literal, myObj.key will return undefined.
+*/
+//forin in Array
+
+
+var myArr = ["js", "python", "java"];
+
+for (var _key in myArr) {
+  console.log("".concat(_key, " : ").concat(myArr[_key]));
+} //*** We can't use it on Map */
--- 05_iterators/four.js
+++ 05_iterators/four.js
@@ -1,21 +1,30 @@
 // ****for in loop
 
 // forin in object
-const myObj={
-    js:"JavaScript",
-    cpp:"C Plus Plus"
-}
+const myObj = {
+  js: "JavaScript",
+  cpp: "C Plus Plus",
+};
 
 for (const key in myObj) {
-   
-   console.log(`${key} means ${myObj[key]}`);
+  //key is a array so i use myObj[key]
+  console.log(`${key} means ${myObj[key]}`);
 }
 
+/*
+console.log(myObj.key)//gives undefined cause there has not key as named "key"
+
+
+If you try to use myObj.key instead, JavaScript will attempt to access a property
+named "key" on the myObj object, rather than dynamically accessing the property 
+whose name is held in the variable key. Since there's no such property named "key"
+in your object literal, myObj.key will return undefined.
+*/
 //forin in Array
-const myArr=['js','python','java'];
+const myArr = ["js", "python", "java"];
 
 for (const key in myArr) {
-    console.log(`${key} : ${myArr[key]}`);
+  console.log(`${key} : ${myArr[key]}`);
 }
 
 //*** We can't use it on Map */
