172885bf053f3eacc82951251818e1ecadd2ddfd
Muizz Rafid <muizzrafid1@gmail.com>
2024-10-03 22:04:02 +0600

    truthy falsy added

--- 04_control_flow/truthy_and_falsy.js
+++ 04_control_flow/truthy_and_falsy.js
@@ -1,10 +1,15 @@
 //what you write mainly belongs to two types.1)truthy2)falsy
 
-//falsy values:0 undefined NaN document.all false null
+//falsy values:0 undefined NaN document.all false null "" -0
 //And all other are truthy
 
-if (-1) {
+if ("") {
   console.log("this is truthy");
 } else {
   console.log("this is falsy");
+}
+
+var array = [1, 2, 3, 4];
+for (var key in array) {
+  console.log(key, array[key]);
 }
