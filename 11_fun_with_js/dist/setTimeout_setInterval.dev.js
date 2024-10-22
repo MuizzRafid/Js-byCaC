3b9bf1188feaa7bc768d5727ae0ce5e21568ef09
Muizz Rafid <muizzrafid1@gmail.com>
2024-10-23 00:06:26 +0600

    learning setInterval

--- 11_fun_with_js/dist/setTimeout_setInterval.dev.js
+++ 11_fun_with_js/dist/setTimeout_setInterval.dev.js
@@ -1,10 +1,15 @@
 "use strict";
 
+function _readOnlyError(name) { throw new Error("\"" + name + "\" is read-only"); }
+
 //if we want to animate any element setInterval is important and there are also many usage. when we set
 //the time of interval it calls again again after that time.to stop setInterval we use clearInterval()
+var counter = 0;
 var interval = setInterval(function () {
   console.log("hellow");
+  counter += (_readOnlyError("counter"), 1);
+
+  if (counter >= 5) {
+    clearInterval(interval);
+  }
 }, 1000);
-setTimeout(function () {
-  clearInterval(interval);
-}, 5000);
--- 11_fun_with_js/setTimeout_setInterval.js
+++ 11_fun_with_js/setTimeout_setInterval.js
@@ -1,10 +1,11 @@
 //if we want to animate any element setInterval is important and there are also many usage. when we set
 //the time of interval it calls again again after that time.to stop setInterval we use clearInterval()
 
+const counter = 0;
 var interval = setInterval(() => {
   console.log("hellow");
+  counter += 1;
+  if (counter >= 5) {
+    clearInterval(interval);
+  }
 }, 1000);
-
-setTimeout(() => {
-  clearInterval(interval);
-}, 5000);
