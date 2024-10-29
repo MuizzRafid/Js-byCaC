f3d8d7d7f0dd494f7c7262f125e3dc0ee5866aae
Muizz Rafid <muizzrafid1@gmail.com>
2024-10-29 07:32:44 +0600

    new exercise added

--- 11_fun_with_js/dist/p_qtn.dev.js
+++ 11_fun_with_js/dist/p_qtn.dev.js
@@ -32,3 +32,50 @@ var formateDate = function formateDate() {
 
 console.log(formateDate()); //04.question: write a javascript program to create a new string adding "New!" in front of a given string.
 //if the given string begins with "New!" already then return the original string.
+
+var myString = "offer gets ready";
+var have = "New!";
+var result = myString.startsWith(have);
+
+if (result) {
+  console.log("ture");
+} else {
+  myString = have.concat(" ", myString);
+} //console.log(myString);
+//another way
+
+
+var addNew = function addNew(str) {
+  return str.indexOf("New!") === 0 ? str : "New! ".concat(str);
+}; //console.log(addNew("New! Offers"));
+//05.question:write a js program to create a new string from a given string taking the first 3 char
+//and the last 3 char of a string and adding them together .The string length must be 3 or more if
+//not , the original string is returned.
+
+
+var str = "july";
+
+if (str.length < 4) {//console.log(str);
+} else {
+  var _result = str.slice(0, 3).concat(str.slice(str.length - 3, str.length)); //console.log(result);
+
+} //another way
+
+
+var makeNewString = function makeNewString(str) {
+  return str.length < 3 ? str : str.slice(0, 3) + str.slice(-3);
+}; //console.log(makeNewString("abcef"));
+//06.question:write a js program to extract the first half of a string of even length.
+
+
+var firstHalf = function firstHalf(str2) {
+  return str2.length % 2 == 0 ? str2.slice(0, str2.length / 2) : "not even length";
+}; //console.log(firstHalf("jjjkdekd"));
+//07.question:write js program to concatenate two string except their first character.
+
+
+var concateTwo = function concateTwo(str1, str2) {
+  return str1.slice(1) + str2.slice(1);
+};
+
+console.log(concateTwo("july", "downey"));
--- 11_fun_with_js/p_qtn.js
+++ 11_fun_with_js/p_qtn.js
@@ -35,3 +35,54 @@ console.log(formateDate());
 
 //04.question: write a javascript program to create a new string adding "New!" in front of a given string.
 //if the given string begins with "New!" already then return the original string.
+
+let myString = "offer gets ready";
+let have = "New!";
+let result = myString.startsWith(have);
+
+if (result) {
+  console.log("ture");
+} else {
+  myString = have.concat(" ", myString);
+}
+
+//console.log(myString);
+
+//another way
+
+const addNew = (str) => (str.indexOf("New!") === 0 ? str : `New! ${str}`);
+
+//console.log(addNew("New! Offers"));
+
+//05.question:write a js program to create a new string from a given string taking the first 3 char
+//and the last 3 char of a string and adding them together .The string length must be 3 or more if
+//not , the original string is returned.
+
+let str = "july";
+
+if (str.length < 4) {
+  //console.log(str);
+} else {
+  let result = str.slice(0, 3).concat(str.slice(str.length - 3, str.length));
+  //console.log(result);
+}
+
+//another way
+
+const makeNewString = (str) =>
+  str.length < 3 ? str : str.slice(0, 3) + str.slice(-3);
+
+//console.log(makeNewString("abcef"));
+
+//06.question:write a js program to extract the first half of a string of even length.
+
+const firstHalf = (str2) =>
+  str2.length % 2 == 0 ? str2.slice(0, str2.length / 2) : "not even length";
+
+//console.log(firstHalf("jjjkdekd"));
+
+//07.question:write js program to concatenate two string except their first character.
+
+const concateTwo = (str1, str2) => str1.slice(1) + str2.slice(1);
+
+console.log(concateTwo("july", "downey"));
